71cc50e0e6eec0ca09d823111268598158a134f0
Ramesh <ramesitexp@gmail.com>
2025-08-31 10:28:11 +0530

    first commit

---
+++ sample_data/app/vectorstore_utils.py
@@ -0,0 +1,11 @@
+from langchain_community.vectorstores import FAISS
+from lanchain_community.embeddings import HuggingFaceEmbeddings
+from typing import List
+
+def create_faiss_index(texts: List[str]) :
+    embeddings = HuggingFaceEmbeddings(model_name = "sentence-transformers/all-mpnet-base-v2")
+    return FAISS.from_texts(texts, embeddings)
+
+
+def retrive_relevant_docs(vectorstore: FAISS, query: str, k: int = 4):
+    return vectorstore.similarity_search(query, k=k)
